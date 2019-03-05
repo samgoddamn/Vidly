04ae6ebf6193f996e67397407b1bb06cdf86d209
Samuel Lucas <samuel@yonglogic.onmicrosoft.com>
2019-03-05 14:22:38 +0100

    Section 3

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        protected readonly List<Customer> customers = new List<Customer> {
+            new Customer { Name = "John Smith", Id = 1 },
+            new Customer { Name = "Mary Williams", Id = 2 }};
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = customers.Find(x => x.Id == id);
+            if (customer != null)
+                return View(customer);
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = customers.Find(x => x.Id == id);
+            if (customer != null)
+                return View(customer);
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+    }
+}
