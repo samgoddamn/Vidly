d898662098964ee30b8bd99e395b798f16dcea6f
Samuel Lucas <samuel@yonglogic.onmicrosoft.com>
2019-03-12 10:29:13 +0100

    Added data annotations to movies

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -13,8 +13,13 @@ namespace Vidly.Models
 
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Date Added")]
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
+
         public Genre Genre { get; set; }
         public byte GenreId { get; set; }
     }
