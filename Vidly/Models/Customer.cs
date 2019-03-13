a4f1102b9fc7f0fbc4944cb164c956917f8c3343
Samuel Lucas <samuel@yonglogic.onmicrosoft.com>
2019-03-13 12:05:06 +0100

    Server-side validation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,12 +9,18 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
