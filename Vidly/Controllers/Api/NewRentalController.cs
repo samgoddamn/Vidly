13c2e9ec6059e31dcf48d2350b6b325c313768d1
Samuel Lucas <samuel@yonglogic.onmicrosoft.com>
2019-08-13 15:02:45 +0200

    Rental API

---
+++ Vidly/Controllers/Api/NewRentalController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+using AutoMapper;
+using System.Data.Entity;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDto rentalDto)
+        {
+            var customer = _context.Customers.Single(
+                c => c.Id == rentalDto.CustomerId);
+            
+            var movies = _context.Movies.Where(
+                m => rentalDto.MovieIds.Contains(m.Id)).ToList();
+
+            foreach (Movie movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental()
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
