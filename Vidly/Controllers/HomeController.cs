b313e4063dd95ce9619af63a4a1279d8bf96a5d7
Samuel Lucas <samuel@yonglogic.onmicrosoft.com>
2019-08-15 10:00:29 +0200

    Error handling

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -16,6 +16,8 @@ namespace Vidly.Controllers
 
         public ActionResult About()
         {
+            throw new HttpException(); 
+
             ViewBag.Message = "Your application description page.";
 
             return View();
